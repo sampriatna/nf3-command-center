238fc39812c754d6451766c0268a5cce7650914e
sampriatna <sampriatna@gmail.com>
2026-05-22 14:33:49 +0700

    Create tailwind.config.ts

---
+++ tailwind.config.ts
@@ -0,0 +1,21 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1E40AF",
+        sidebar: "#1E293B",
+        "sidebar-hover": "#334155",
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
